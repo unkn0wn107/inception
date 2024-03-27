7aa5d74960320af0e4d5976f1e76e4953ec2b3d7
agaley <agaley@student.42lyon.fr>
2024-03-27 18:13:30 +0100

    [Certs] Remove readonly, change to 4096

--- srcs/requirements/nginx/tools/gen-cert.sh
+++ srcs/requirements/nginx/tools/gen-cert.sh
@@ -6,7 +6,7 @@ KEY_FILE="${CERT_DIR}/${DOMAIN_NAME}.key"
 CRT_FILE="${CERT_DIR}/${DOMAIN_NAME}.crt"
 
 if [ ! -f "$KEY_FILE" ] || [ ! -f "$CRT_FILE" ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    sudo openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
         -keyout "$KEY_FILE" \
         -out "$CRT_FILE" \
         -subj "/C=FR/ST=Rhône-Alpes/L=Lyon/O=42/CN=${DOMAIN_NAME}"
