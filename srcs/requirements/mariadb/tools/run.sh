589213a3335cfe0927eba8464eb2788fa93388f0
agaley <agaley@student.42lyon.fr>
2024-04-05 02:52:39 +0200

    [DB] Save Mariadb config

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -4,12 +4,13 @@ set -ux
 
 mariadbd --bootstrap --skip-grant-tables=0 --datadir=/var/lib/mysql <<EOF
 DROP DATABASE IF EXISTS test;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
-GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${DB_ROOT_PASS}');
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\` CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+GRANT ALL ON \`${DB_NAME}\`.* TO '${DB_USER}'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
-exec mariadbd -v --user=mysql --console
+exec mariadbd -v --user=mysql --datadir=/var/lib/mysql --console
